b14ff881c870d21d912b0bf9187378bd714a4250
HarleyAppleChoi <appleno56@gmail.com>
2021-12-14 17:39:47 +0000

    init commit

---
+++ blocks.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+
+	logger "github.com/ElrondNetwork/elrond-go-logger"
+	"github.com/ElrondNetwork/elrond-sdk-erdgo/blockchain"
+	"github.com/ElrondNetwork/elrond-sdk-erdgo/examples"
+)
+
+
+func main(){
+
+}
